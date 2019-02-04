26b601d259e134aefb0cc3899bef793f56c87f7c
Jeff Bass <jeffbass42@gmail.com>
2019-02-04 12:22:02 -0800

    Update __version__.py

---
+++ yyr/__version__.py
@@ -0,0 +1,9 @@
+# populates fields for >>>help(imagezmq)
+__title__ = 'yin-yang-ranch'
+__description__ = 'Overall Project Design for Yin Yang Ranch'
+__url__ = 'https://github.com/jeffbass/yin-yang-ranch'
+__version__ = '0.0.5'
+__author__ = 'Jeff Bass'
+__author_email__ = 'jeff@yin-yan-ranch.com'
+__license__ = 'MIT 1.0'
+__copyright__ = 'Copyright 2019 Jeff Bass'
