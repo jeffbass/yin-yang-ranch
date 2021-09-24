3ae926fe8e21096bc03bc26745998574f3de7501
Jeff Bass <jeff@yin-yang-ranch.com>
2021-09-24 11:36:52 -0700

    remove redundant yyr files

--- yyr/__version__.py
+++
@@ -1,9 +0,0 @@
-# populates fields for >>>help(imagezmq)
-__title__ = 'yin-yang-ranch'
-__description__ = 'Overall Project Design for Yin Yang Ranch'
-__url__ = 'https://github.com/jeffbass/yin-yang-ranch'
-__version__ = '0.0.6'
-__author__ = 'Jeff Bass'
-__author_email__ = 'jeff@yin-yan-ranch.com'
-__license__ = 'MIT'
-__copyright__ = 'Copyright 2020 Jeff Bass'
